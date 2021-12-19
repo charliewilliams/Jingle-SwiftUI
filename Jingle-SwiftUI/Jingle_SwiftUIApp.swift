7cf942ea79eb31c0a2410278cd1aca25b9c96dd2
Charlie Williams <c@harl.ie>
2021-12-19 22:19:51 +0000

    Initial Commit

---
+++ Jingle-SwiftUI/Jingle_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Jingle_SwiftUIApp.swift
+//  Jingle-SwiftUI
+//
+//  Created by Charlie Williams on 19/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Jingle_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
